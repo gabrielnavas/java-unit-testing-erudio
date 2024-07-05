08b321852e621964b591d8938e16592ede7af77d
navas <gabrielnavas@email.com>
2024-07-05 18:36:09 -0300

    math sum test

---
+++ section-3/automated-tests-with-java/src/main/java/io/github/gabrielnavas/automated_tests_with_java/SimpleMath.java
@@ -0,0 +1,28 @@
+package io.github.gabrielnavas.automated_tests_with_java;
+
+public class SimpleMath {
+
+    public Double sum(Double first, Double second) {
+        return first + second;
+    }
+
+    public Double subtract(Double first, Double second) {
+        return first - second;
+    }
+
+    public Double multiply(Double first, Double second) {
+        return first * second;
+    }
+
+    public Double divide(Double first, Double second) {
+        return first / second;
+    }
+
+    public Double mean(Double first, Double second) {
+        return (first + second) / 2;
+    }
+
+    public Double sqrt(Double number) {
+        return Math.sqrt(number);
+    }
+}
