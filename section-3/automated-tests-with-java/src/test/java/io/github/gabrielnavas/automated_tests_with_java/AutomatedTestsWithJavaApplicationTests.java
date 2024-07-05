eb8284e67d691b99e31bddfe37aa6642ccb626a5
navas <gabrielnavas@email.com>
2024-07-05 18:09:32 -0300

    init section 3

---
+++ section-3/automated-tests-with-java/src/test/java/io/github/gabrielnavas/automated_tests_with_java/AutomatedTestsWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielnavas.automated_tests_with_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestsWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
