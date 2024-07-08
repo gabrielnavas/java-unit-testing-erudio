d8fc1627316dca84109e6c27d290e168a5066c10
navas <gabrielnavas@email.com>
2024-07-08 17:45:41 -0300

    testing performance by milliseconds

--- section-3/automated-tests-with-java/src/test/java/io/github/gabrielnavas/automated_tests_with_java/math/ArraysCompareTest.java
+++ section-3/automated-tests-with-java/src/test/java/io/github/gabrielnavas/automated_tests_with_java/math/ArraysCompareTest.java
@@ -2,8 +2,10 @@ package io.github.gabrielnavas.automated_tests_with_java.math;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class ArraysCompareTest {
 
@@ -18,5 +20,18 @@ public class ArraysCompareTest {
 
         // Then
         Assertions.assertArrayEquals(expectedArray, numbers);
+    }
+
+    @Test
+    @Timeout(value = 20, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        // Given
+        int[] numbers = {25, 8, 21, 32, 3};
+
+        // When
+        for (int i = 0; i < 1_000_000; i++) {
+            numbers[0] = 0;
+            Arrays.sort(numbers);
+        }
     }
 }
