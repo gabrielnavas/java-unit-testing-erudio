8c65b5d7850b998a0e3126d1b39dcea0ce12ec78
navas <gabrielnavas@email.com>
2024-07-08 14:03:13 -0300

    test math

--- section-3/automated-tests-with-java/src/test/java/io/github/gabrielnavas/automated_tests_with_java/math/SimpleMathTest.java
+++ section-3/automated-tests-with-java/src/test/java/io/github/gabrielnavas/automated_tests_with_java/math/SimpleMathTest.java
@@ -20,4 +20,79 @@ public class SimpleMathTest {
                         first, second, actual, expected)
         );
     }
+
+    @Test
+    public void testSubtract() {
+        SimpleMath math = new SimpleMath();
+        double first = 4;
+        double second = 2;
+        Double actual = math.subtract(first, second);
+        double expected = 2.0D;
+        Assertions.assertEquals(
+                expected,
+                actual,
+                () -> String.format("The math.subtract(%.2f, %.2f)=%.2f did not produced expected value %.2f.",
+                        first, second, actual, expected)
+        );
+    }
+
+
+    @Test
+    public void testMultiply() {
+        SimpleMath math = new SimpleMath();
+        double first = 4;
+        double second = 2;
+        Double actual = math.multiply(first, second);
+        double expected = 8.0D;
+        Assertions.assertEquals(
+                expected,
+                actual,
+                () -> String.format("The math.multiply(%.2f, %.2f)=%.2f did not produced expected value %.2f.",
+                        first, second, actual, expected)
+        );
+    }
+
+    @Test
+    public void testDivide() {
+        SimpleMath math = new SimpleMath();
+        double first = 4;
+        double second = 2;
+        Double actual = math.divide(first, second);
+        double expected = 2.0D;
+        Assertions.assertEquals(
+                expected,
+                actual,
+                () -> String.format("The math.multiply(%.2f, %.2f)=%.2f did not produced expected value %.2f.",
+                        first, second, actual, expected)
+        );
+    }
+
+    @Test
+    public void testSqrt() {
+        SimpleMath math = new SimpleMath();
+        double n = 81;
+        Double actual = math.sqrt(n);
+        double expected = 9.0D;
+        Assertions.assertEquals(
+                expected,
+                actual,
+                () -> String.format("The math.sqrt(%.2f)=%.2f did not produced expected value %.2f.",
+                        n, actual, expected)
+        );
+    }
+
+    @Test
+    public void testMean() {
+        SimpleMath math = new SimpleMath();
+        double first = 4;
+        double second = 2;
+        Double actual = math.mean(first, second);
+        double expected = 3.0D;
+        Assertions.assertEquals(
+                expected,
+                actual,
+                () -> String.format("The math.mean(%.2f, %.2f)=%.2f did not produced expected value %.2f.",
+                        first, second, actual, expected)
+        );
+    }
 }
