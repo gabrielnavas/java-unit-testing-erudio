3d918ae4053e6c92155c29a8eebb68ee82fe5f7b
navas <gabrielnavas@email.com>
2024-07-11 17:28:49 -0300

    init project with mockito and hancrest

---
+++ section-6/first-steps-mockito/src/main/java/io/github/gabrielnavas/first_steps_mockito/FirstStepsMockitoApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielnavas.first_steps_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepsMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStepsMockitoApplication.class, args);
+	}
+
+}
---
+++ section-6/first-steps-mockito/src/test/java/io/github/gabrielnavas/first_steps_mockito/FirstStepsMockitoApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.gabrielnavas.first_steps_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepsMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
