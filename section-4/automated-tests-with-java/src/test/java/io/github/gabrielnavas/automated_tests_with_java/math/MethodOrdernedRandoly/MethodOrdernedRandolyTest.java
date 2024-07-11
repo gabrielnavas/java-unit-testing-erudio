4b826c87128c26f2f73e17b1d38675b2023b4260
navas <gabrielnavas@email.com>
2024-07-10 22:48:28 -0300

    method orderned randoly test

---
+++ section-4/automated-tests-with-java/src/test/java/io/github/gabrielnavas/automated_tests_with_java/math/MethodOrdernedRandoly/MethodOrdernedRandolyTest.java
@@ -0,0 +1,30 @@
+package io.github.gabrielnavas.automated_tests_with_java.math.MethodOrdernedRandoly;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrdernedRandolyTest {
+
+    @Test
+    void testA() {
+        System.out.println("Test A");
+    }
+
+    @Test
+    void testB() {
+        System.out.println("Test B");
+    }
+
+    @Test
+    void testC() {
+        System.out.println("Test C");
+    }
+
+    @Test
+    void testD() {
+        System.out.println("Test D");
+    }
+
+}
