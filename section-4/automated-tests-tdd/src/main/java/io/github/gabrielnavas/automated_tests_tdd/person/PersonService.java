eae76a61ea442dec5cb0f149c516f3bc49ed78ae
navas <gabrielnavas@email.com>
2024-07-11 16:59:15 -0300

    test the email field

--- section-4/automated-tests-tdd/src/main/java/io/github/gabrielnavas/automated_tests_tdd/person/PersonService.java
+++ section-4/automated-tests-tdd/src/main/java/io/github/gabrielnavas/automated_tests_tdd/person/PersonService.java
@@ -5,6 +5,13 @@ import java.util.concurrent.atomic.AtomicLong;
 public class PersonService implements IPersonService {
     @Override
     public Person createPerson(Person person) {
+        if(person.getEmail() == null) {
+            throw new IllegalArgumentException("Email is null");
+        }
+        if(person.getEmail().isBlank()) {
+            throw new IllegalArgumentException("Email is empty");
+        }
+
         var id = new AtomicLong().incrementAndGet();
         return new Person(
                 id,
--- section-4/automated-tests-tdd/src/test/java/io/github/gabrielnavas/automated_tests_tdd/PersonServiceTest.java
+++ section-4/automated-tests-tdd/src/test/java/io/github/gabrielnavas/automated_tests_tdd/PersonServiceTest.java
@@ -7,10 +7,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class PersonServiceTest {
 
     Person person;
+    IPersonService service;
 
     @BeforeEach
     void setup() {
@@ -22,14 +28,12 @@ public class PersonServiceTest {
                 "12 Street - Uk",
                 "Male"
         );
+        service = new PersonService();
     }
 
     @DisplayName("When Create a Person with Success Should Return a Person Object With Not Null Field")
     @Test
     public void testCreatePerson_WhenSuccess_ShouldReturnPersonObjectWithNotNullField() {
-        // Given / Arrange
-        IPersonService service = new PersonService();
-
         // When / Act
         Person actual = service.createPerson(person);
 
@@ -46,9 +50,6 @@ public class PersonServiceTest {
     @DisplayName("When Create a Person with Success Should Contains Valid Fields In Returned a Person Object")
     @Test
     public void testCreatePerson_WhenSuccess_ShouldContainsValidFieldsInReturnedPersonObject() {
-        // Given / Arrange
-        IPersonService service = new PersonService();
-
         // When / Act
         Person actual = service.createPerson(person);
 
@@ -59,5 +60,44 @@ public class PersonServiceTest {
         Assertions.assertEquals(actual.getStreet(), person.getStreet(), () -> "The Person Street is Incorrect");
         Assertions.assertEquals(actual.getGender(), person.getGender(), () -> "The Person Gender is Incorrect");
     }
+
+
+    @DisplayName("When Create a Person with Null Email Should Throws Exception")
+    @ParameterizedTest
+    @MethodSource("testEmailInvalidParameters")
+    public void testCreatePerson_WhichNullEmail_ShouldThrowsIllegalArgumentException(String email, String expectedMessage) {
+        // Given / Arrange
+        person = new Person(
+                1L,
+                "John",
+                "Neth",
+                email,
+                "12 Street - Uk",
+                "Male"
+        );
+
+        // When & Then
+        Exception actual = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> service.createPerson(person),
+                () -> String.format("The Person Email Should throws a %s exception", IllegalArgumentException.class.getName()));
+
+        // Then
+        Assertions.assertEquals(
+                expectedMessage,
+                actual.getMessage(),
+                () -> String.format("The Person Email Should return a %s exception", IllegalArgumentException.class.getName())
+        );
+    }
+
+
+    public static Stream<Arguments> testEmailInvalidParameters() {
+        return Stream.of(
+                Arguments.of(null, "Email is null"),
+                Arguments.of("", "Email is empty"),
+                Arguments.of(" ", "Email is empty")
+        );
+    }
+
 }
 
