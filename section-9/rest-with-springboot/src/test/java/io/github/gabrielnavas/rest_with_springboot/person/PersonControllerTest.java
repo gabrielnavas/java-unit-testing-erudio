403ccab0beff2742e50722d0b7fe8c119d0c9715
navas <gabrielnavas@email.com>
2024-07-24 17:03:19 -0300

    test find all by query controller

--- section-9/rest-with-springboot/src/test/java/io/github/gabrielnavas/rest_with_springboot/person/PersonControllerTest.java
+++ section-9/rest-with-springboot/src/test/java/io/github/gabrielnavas/rest_with_springboot/person/PersonControllerTest.java
@@ -11,9 +11,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -60,5 +64,29 @@ public class PersonControllerTest {
                 .andExpect(jsonPath("$.email", is(personRequest.getEmail())))
                 .andExpect(jsonPath("$.address", is(personRequest.getAddress())))
                 .andExpect(jsonPath("$.gender", is(personRequest.getGender())));
+    }
+
+
+    @DisplayName("")
+    @Test
+    void testGivenListPersons_WhenFindAllByQuery_thenReturnListPersons() throws Exception {
+        // Given
+        List<Person> personList = new ArrayList<>();
+        personList.add(person);
+
+        // When
+        when(personService.findAllPerson(anyInt(), anyInt(), anyString())).thenReturn(personList);
+        ResultActions resultActions = mockMvc.perform(
+                get("/person").accept(MediaType.APPLICATION_JSON)
+        );
+
+        // Then
+        resultActions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].firstName", is(personRequest.getFirstName())))
+                .andExpect(jsonPath("$[0].lastName", is(personRequest.getLastName())))
+                .andExpect(jsonPath("$[0].email", is(personRequest.getEmail())))
+                .andExpect(jsonPath("$[0].address", is(personRequest.getAddress())))
+                .andExpect(jsonPath("$[0].gender", is(personRequest.getGender())));
     }
 }
